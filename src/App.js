5260a8b38a26a95634a1e07d80b7d088e735760d
Jacob Meyer <jakem3350@gmail.com>
2023-07-04 00:55:45 -0400

    Layed out structure for nav and main sections
    - Added page links to nav
    - Fixed profile icon positioning
    - Added small scrollable column to main
    - Added big main section to main

--- src/App.js
+++ src/App.js
@@ -5,7 +5,27 @@ function App() {
   return (
       <body>
       <Nav></Nav>
-      <div></div>
+      <main>
+          <div className={"row"} style={{marginLeft:"20px"}}>
+            <div className={"col-4 main-col-sm"}>
+                <div className={"main-cont-sm"}>Content</div>
+                <div className={"main-cont-sm"}>Content</div>
+                <div className={"main-cont-sm"}>Joe mama</div>
+                <div className={"main-cont-sm"}>Bryan is still iron</div>
+                <div className={"main-cont-sm"}>yuh</div>
+                <div className={"main-cont-sm"}>more content</div>
+                <div className={"main-cont-sm"}>more more content</div>
+                <div className={"main-cont-sm"}></div>
+                <div className={"main-cont-sm"}></div>
+                <div className={"main-cont-sm"}></div>
+                <div className={"main-cont-sm"}></div>
+                <div className={"main-cont-sm"}></div>
+            </div>
+            <div className={"col-8"}>
+                <div className={"main-cont-lg"}>main container</div>
+            </div>
+          </div>
+      </main>
 
       </body>
   );
--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,13 @@
 export default function Nav() {
     return(
-        <nav>
-            <div className={"profile-icon"}><b>J</b></div>
+        <nav className={"d-flex"}>
+            <div className={"d-flex"} style={{width:"95%"}}>
+                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                <span style={{marginRight:"70px"}}><a className={"link"} href={"#"}><h5>Dashboard</h5></a></span>
+                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                <span><a className={"link"} href={"#"}><h5>My Goals</h5></a></span>
+            </div>
+            <span className={"profile-icon"}><b>J</b></span>
         </nav>
     );
 
