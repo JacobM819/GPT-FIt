48feab9474e01a24526bd003ba81d5c1b5ceb143
Jacob Meyer <jakem3350@gmail.com>
2023-07-08 21:12:08 -0400

    Started nav profile popup
    - Added card that pops up when clicking on profile

--- src/Nav.js
+++ src/Nav.js
@@ -1,4 +1,12 @@
 export default function Nav() {
+
+    // let profile = document.getElementById("profile-icon");
+    function hideDropdown() {
+        let profile = document.getElementById("profile-icon")
+        profile.hidden = profile.hidden !== true;
+    }
+    // document.getElementById("profile-icon").addEventListener("click", hideDropdown)
+
     return(
         <nav className={"d-flex"}>
             <div className={"d-flex"} style={{width:"95%"}}>
@@ -7,7 +15,12 @@ export default function Nav() {
                 {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
                 <span><a className={"link"} href={"#"}><h5>My Goals</h5></a></span>
             </div>
-            <span className={"profile-icon"}><b>J</b></span>
+            <span id={"profile-icon"}><b>J</b></span>
+            <div className={"dropdown"}>
+                    <a href={"#"}>Profile</a>
+                    <hr/>
+                    <a href={"#"}>Logout</a>
+            </div>
         </nav>
     );
 
